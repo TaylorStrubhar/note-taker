0b413099d63ed97d8fcb443a92b572eac250a369
Ira Strubhar <taylor.strubhar@gmail.com>
2022-07-17 18:56:22 -0700

    real last try, i think I found it.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 // parsing with express
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // static files with express
 app.use(express.static('public'));
