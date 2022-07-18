a4e2ff0bead383095e93a289fed1f2ba8ae01836
Ira Strubhar <taylor.strubhar@gmail.com>
2022-07-17 18:33:31 -0700

    one last try!

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = (app) => {
     app.get("/notes", (req, res) => {
-        res.sendFile(path.join(_dirname, '../public/notes.html'));
+        res.sendFile(path.join(_dirname, "../public/notes.html"));
     });
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(_dirname, '../public/index.html'));
+        res.sendFile(path.join(_dirname, "../public/index.html"));
     });
 }
